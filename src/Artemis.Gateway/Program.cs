599fc4fb7277cb85efeaca90dc0ed5745e805160
Onur <onurarifciftci@gmail.com>
2026-02-10 22:44:18 +0300

    ocelot.json

--- src/Artemis.Gateway/Program.cs
+++ src/Artemis.Gateway/Program.cs
@@ -3,7 +3,12 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+// Development ortamında ocelot.Development.json, diğer ortamlarda ocelot.json kullan
+var ocelotFile = builder.Environment.IsDevelopment() 
+    ? "ocelot.Development.json" 
+    : "ocelot.json";
+
+builder.Configuration.AddJsonFile(ocelotFile, optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
 builder.Services.AddCors(options =>
